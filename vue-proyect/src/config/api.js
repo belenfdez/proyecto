b7068c63d3eb8558be997ac0b7deca6b2116748c
belenfdez <126102803+belenfdez@users.noreply.github.com>
2024-12-10 18:08:11 +0100

    Delete vue-proyect/src/config/api.js

--- vue-proyect/src/config/api.js
+++
@@ -1,7 +0,0 @@
-// src/config/api.js
-import axios from 'axios';
-
-export const api = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL, // Usamos la variable de entorno para la URL base
-  timeout: 5000,  // Tiempo máximo para la petición
-});
